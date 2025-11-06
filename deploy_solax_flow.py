d747fac8fc91ed0d24f024bce564dc34254a1980
Jaideep-design <jaideep.gupta@ecozensolutions.com>
2025-11-06 11:48:23 +0530

    Add files via upload

--- deploy_solax_flow.py
+++ deploy_solax_flow.py
@@ -4,19 +4,19 @@ Created on Thu Nov  6 11:19:08 2025
 
 @author: Admin
 """
-
+from prefect_github import GitHubRepository
 from prefect_wrapper import solax_flow
-from prefect.filesystems import GitHub
 
 if __name__ == "__main__":
-    # point to your repo
-    github_block = GitHub.load("solax-code-storage")
+    # Load your GitHub storage block
+    github_block = GitHubRepository.load("solax-code-storage")  # your block name
 
+    # Deploy the flow
     solax_flow.deploy(
         name="solax-poc",
         work_pool_name="Default_poc_prefect_managed",
-        schedule={"cron": "*/10 * * * *", "timezone": "UTC"},
+        schedule={"cron": "*/10 * * * *", "timezone": "Asia/Kolkata"},
         description="Solax push job running every 10 minutes via Prefect Cloud",
         tags=["solax", "poc"],
-        storage=github_block,  # 👈 tells Prefect where to pull your code from
+        storage=github_block,
     )
